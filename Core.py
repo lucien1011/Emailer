189d169236d9c8b7213263184fb5dc4b40fb5a81
lucien1011 <lucien1011@gmail.com>
2019-02-21 11:55:58 -0500

    Can email multpile people

--- Core.py
+++ Core.py
@@ -7,17 +7,20 @@ class Emailer(object):
         self.msg = MIMEMultipart()
         self.server = smtplib.SMTP("localhost")
 
-    def sendMail(self,sdAddr,toAddr,subject,main):
-        self.msg = MIMEMultipart()
-        self.msg['From'] = sdAddr
-        self.msg['To'] = toAddr
-        self.msg['Subject'] = subject 
-        self.msg.attach(MIMEText(main, 'plain'))
+    def sendMail(self,sdAddr,toAddrs,subject,main):
+        if type(toAddrs) != list: toAddrs = [toAddrs]
+        for toAddr in toAddrs:
+            self.msg = MIMEMultipart()
+            self.msg['From'] = sdAddr
+            self.msg['To'] = toAddr
+            self.msg['Subject'] = subject 
+            self.msg.attach(MIMEText(main, 'plain'))
 
-        text = self.msg.as_string()
+            text = self.msg.as_string()
 
-        self.server = smtplib.SMTP("localhost")
-        self.server.sendmail(sdAddr, toAddr, text)
+            self.server = smtplib.SMTP("localhost")
+
+            self.server.sendmail(sdAddr, toAddr, text)
 
     def getUserName(self):
         return str(os.environ['USER'])
