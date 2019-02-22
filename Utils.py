a836828baa71e132d530db3fc01cbfd5d361380a
lucien1011 <lucien1011@gmail.com>
2019-02-22 06:17:28 -0500

    Bug fix

--- Utils.py
+++ Utils.py
@@ -1,11 +1,17 @@
 from .Core import Emailer
 import os
 
+emailer = Emailer()
+
 def sendQuickMail(sdAttrs,title,main):
-    emailer = Emailer()
+    #emailer = Emailer()
     emailer.sendMail(
-            os.environ["USERNAME"]+"@"+os.environ["HOSTNAME"],
+            os.environ["USER"]+"@"+os.environ["HOSTNAME"],
             sdAttrs,
             title,
             main,
             )
+
+def getTimeStamp():
+    #emailer = Emailer()
+    return emailer.getTimeStr()
