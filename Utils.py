b2d3a9d2615c7a32c898500233eb610c0337571e
lucien1011 <lucien1011@gmail.com>
2019-03-04 06:28:16 -0500

    added function to get date diff

--- Utils.py
+++ Utils.py
@@ -1,5 +1,6 @@
 from .Core import Emailer
 import os
+from datetime import datetime
 
 emailer = Emailer()
 
@@ -13,5 +14,15 @@ def sendQuickMail(sdAttrs,title,main):
             )
 
 def getTimeStamp():
-    #emailer = Emailer()
-    return emailer.getTimeStr()
+    curTime=(time.strftime("%H:%M:%S"))
+    curDate=(time.strftime("%d/%m/%Y"))
+    return curDate+" ; "+curTime 
+
+def getDateDiff(d1Str,d2Str,d_format,mk_str=False):
+    a = datetime.strptime(d1, date_format)
+    b = datetime.strptime(d2, date_format)
+    delta = b - a
+    if mk_str and delta.days == 1: 
+        return "tomorrow"
+    else:
+        return delta.days+" days"
